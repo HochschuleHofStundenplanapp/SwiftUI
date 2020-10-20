014a7e22343de1c7fae0848d7f772bf866b97097
Andre Klein <mail@ankler.de>
2020-10-20 15:15:17 +0200

    init

--- CodeSnippets/Xcode/CombinePipeline/CombinePipeline/ContentView.swift
+++ CodeSnippets/Xcode/CombinePipeline/CombinePipeline/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        VStack {
         Text("Hello, world!")
             .padding()
+        }.onAppear {
+            Pipelines().output()
+        }
     }
 }
 
---
+++ CodeSnippets/Xcode/CombinePipeline/CombinePipeline/Pipelines.swift
@@ -0,0 +1,14 @@
+//
+//  Pipelines.swift
+//  CombinePipeline
+//
+//  Created by Andre Klein (privat) on 20.10.20.
+//
+
+import Foundation
+
+struct Pipelines {
+    func output() {
+        print("Hello")
+    }
+}
