b1af5c934823e7bfd591b0d0a699b9365174ba2b
Xiongwei Zheng <zhengxiongwei@web.de>
2020-10-20 15:05:23 +0200

    CombinePipeline Projekt

---
+++ CodeSnippets/Xcode/CombinePipeline/CombinePipeline/CombinePipelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePipelineApp.swift
+//  CombinePipeline
+//
+//  Created by Xiongwei Zheng on 20.10.20.
+//
+
+import SwiftUI
+
+@main
+struct CombinePipelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
