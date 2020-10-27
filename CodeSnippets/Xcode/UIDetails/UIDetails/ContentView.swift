6f4b099bdc2c6dff7fb396d86027434dc1316366
Kay Wilfert <kay.wilfert@gmail.com>
2020-10-27 14:59:32 +0100

    Created new Project for designing a rudimentary User Interface

---
+++ CodeSnippets/Xcode/UIDetails/UIDetails/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  UIDetails
+//
+//  Created by MACBOOK on 27.10.20.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        animation: .default)
+    private var items: FetchedResults<Item>
+
+    var body: some View {
+        Text("HELLO WORLD")
+    }
+}
+
+    
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
