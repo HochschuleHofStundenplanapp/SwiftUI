3d99a4f2d560d57de1d67fc8897b933e30e59e65
Kay Wilfert <kay.wilfert@gmail.com>
2020-10-27 16:32:18 +0100

    Rudimentary first screen with navigation that somehow does not work (yet)

--- CodeSnippets/Xcode/UIDetails/UIDetails/ContentView.swift
+++ CodeSnippets/Xcode/UIDetails/UIDetails/ContentView.swift
@@ -10,18 +10,60 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
+    @State private var terms:[String] = ["NONE","DUMMY 1", "DUMMY 2"]
+    @State private var selected = 0
+    
+    
     var body: some View {
-        Text("HELLO WORLD")
+        NavigationView{
+            VStack {
+                Text("Select a term").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Picker(selection: $selected, label:     Text("Select a term")){
+                    ForEach( 0 ..< terms.count){
+                        Text(self.terms[$0])
+                    }
+                }
+                
+                NavigationLink(
+                    destination: SecondView() ){
+                    Button(action:{}){
+                        Text("Next").font(.title)
+                    }
+                }.buttonStyle(PlainButtonStyle())
+            }
+        }
     }
 }
 
+struct SecondView: View{
     
+    @State private var terms:[String] = ["NONE","DUMMY 1", "DUMMY 2"]
+    @State private var selected = 0
+    
+    var body: some View{
+        VStack {
+            Text("Select a course of studies").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            Picker(selection: $selected, label:     Text("Select a course of studies")){
+                ForEach( 0 ..< terms.count){
+                    Text(self.terms[$0])
+                }
+            }
+            NavigationLink(
+                destination: Text("Screen 3") ){
+                Button( action:{ }){
+                    Text("Next")
+                }
+            }.buttonStyle(PlainButtonStyle())
+            
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
