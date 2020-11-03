2fac29bd58e184e52fcdd86b49d1f90ad812c056
Eric <eric.muench@hof-university.de>
2020-11-03 15:58:26 +0100

    added model files + combine pipelines

---
+++ Projekt/StundenplanApp/StundenplanApp/Models/ServerModel/Courses/Course.swift
@@ -0,0 +1,14 @@
+//
+//  Course.swift
+//  CombinePipeline
+//
+//  Created by Andre Klein (privat) on 20.10.20.
+//
+
+import Foundation
+
+struct Course: Codable {
+    let course, year: String
+    let labels: Labels
+    let semester: [String]
+}
