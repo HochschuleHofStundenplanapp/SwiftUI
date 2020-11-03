36f30050e8a3861ff38a45c7757acea4384249e7
Eric <eric.muench@hof-university.de>
2020-11-03 14:47:57 +0100

    initial commit for final project
    Please do not push .DS_Store files

---
+++ Projekt/StundenplanApp/StundenplanApp/StundenplanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StundenplanAppApp.swift
+//  StundenplanApp
+//
+//  Created by Eric Münch on 03.11.20.
+//
+
+import SwiftUI
+
+@main
+struct StundenplanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
