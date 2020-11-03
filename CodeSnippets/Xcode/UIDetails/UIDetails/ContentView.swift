dd555e0cf26633cc7f0f222d6867088b9d278d22
Kay Wilfert <kay.wilfert@gmail.com>
2020-11-03 13:40:53 +0100

    Documented code a bit so the purpose of parameters and variables is clearer. Values are now passed to next View. "Next" Button only appears after selection. Combine pipelines have yet to be connected. Probably today during this lesson.

--- CodeSnippets/Xcode/UIDetails/UIDetails/ContentView.swift
+++ CodeSnippets/Xcode/UIDetails/UIDetails/ContentView.swift
@@ -15,23 +15,29 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-    @State private var terms:[String] = ["NONE","DUMMY 1", "DUMMY 2"]
+    
+    
+    @State private var terms:[String] = ["-","Sommersemester", "Wintersemester"]
     @State private var selected = 0
+    
+    //term data to send to server
+    let termData = ["SS","WS"]
     
     
     var body: some View {
         NavigationView{
             VStack {
-                Text("Select a term").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Text("Wähle eine Vorlesungszeit").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                 Picker(selection: $selected, label:     Text("Select a term")){
                     ForEach( 0 ..< terms.count){
                         Text(self.terms[$0])
                     }
                 }
-                
-                NavigationLink(destination: SecondView() ){
+                if(selected > 0){
+                NavigationLink(destination: SecondView(term: termData[selected - 1]) ){
                         Text("Next").font(.title)
                 }.buttonStyle(PlainButtonStyle())
+                }
             }
         }
     }
@@ -39,22 +45,67 @@ struct ContentView: View {
 
 struct SecondView: View{
     
-    @State private var terms:[String] = ["NONE","DUMMY 1", "DUMMY 2"]
-    @State private var selected = 0
+    var term:String
+    
+    //course of studies
+    @State private var courses:[String] = []
+    @State private var selectedCourse = 0
+    
+    //available semesters
+    @State private var semesters:[String] = []
+    @State private var selectedSemester = 0
     
     var body: some View{
         VStack {
-            Text("Select a course of studies").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            Picker(selection: $selected, label:     Text("Select a course of studies")){
-                ForEach( 0 ..< terms.count){
-                    Text(self.terms[$0])
+            Text("Wähle einen Studiengang").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            Picker(selection: $selectedCourse, label:     Text("Select a course of studies")){
+                ForEach( 0 ..< courses.count){
+                    Text(self.courses[$0])
                 }
             }
-            NavigationLink( destination: Text("Screen 3") ){
+            
+            Text("Wähle ein Semester").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            Picker(selection: $selectedSemester, label:     Text("Select a course of studies")){
+                ForEach( 0 ..< semesters.count){
+                    Text(self.semesters[$0])
+                }
+            }.onChange(of: selectedCourse, perform: { value in
+                //fetch semesters for parameter
+                //term + course (?)
+            })
+            
+            if(selectedCourse > 0 && selectedSemester > 0){
+            NavigationLink( destination: ThirdView(term:  term, courseOfStudies: courses[selectedCourse], semester: semesters[selectedSemester] ) ){
                 Text("Next").font(.title)
             }.buttonStyle(PlainButtonStyle())
+            }
             
-        }
+        }.onAppear(perform: {
+            //Fetch available courses and semesters using one parameter
+            //term
+        })
+    }
+}
+
+struct ThirdView: View{
+    var term: String
+    var courseOfStudies: String
+    var semester: String
+    
+    var body: some View{
+        VStack{
+            Text("Lesson")
+        }.onAppear(perform: {
+            //fetch courses via all three parameters
+            //term + course of studies + semester
+        })
+    }
+}
+
+struct ListElement: View {
+    // list element
+    var body: some View{
+        Text("")
     }
 }
 
