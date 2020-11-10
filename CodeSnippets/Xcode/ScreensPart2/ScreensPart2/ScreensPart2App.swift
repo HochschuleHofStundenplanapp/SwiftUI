b9aa040f57a46380bc75f7bf5f8581c54cf198be
Xiongwei Zheng <zhengxiongwei@web.de>
2020-11-10 15:45:31 +0100

    ScreensPart2: Initial Commit

---
+++ CodeSnippets/Xcode/ScreensPart2/ScreensPart2/ScreensPart2App.swift
@@ -0,0 +1,17 @@
+//
+//  ScreensPart2App.swift
+//  ScreensPart2
+//
+//  Created by Xiongwei Zheng on 10.11.20.
+//
+
+import SwiftUI
+
+@main
+struct ScreensPart2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
