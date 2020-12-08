94a78651d4863ceb829c89721bf20a3cc1cd36c5
mjakob3 <marvin.jakob@hof-university.de>
2020-12-08 17:07:17 +0100

    added function to get all Dates for a CalendarWeek

--- CodeSnippets/PureSwift/LectureAI/Tests/LectureAITests/LectureAITests.swift
+++ CodeSnippets/PureSwift/LectureAI/Tests/LectureAITests/LectureAITests.swift
@@ -102,6 +102,20 @@ final class LectureAITests: XCTestCase {
         XCTAssertEqual(otherDates, dateUtil.stringify(dates, format: "yyyy-MM-dd"))
     }
 
+    func test_dateOfKW(){
+        let datesOfKw50 = lectureAI.getDatesInCalendarWeek(kw: "50")
+
+        let actualDatesOfKw50 = ["2020-12-07", "2020-12-08", "2020-12-09", "2020-12-10", "2020-12-11",
+                                "2020-12-12", "2020-12-13"]
+
+        XCTAssertEqual(dateUtil.stringify(datesOfKw50), actualDatesOfKw50)
+    }
+
+    func test_currentKW(){
+        let testBool = lectureAI.isLectureKWBeforeCurrentKW(kw: "49")
+        XCTAssertEqual(testBool, true)
+    }
+
     static var allTests = [
         ("testParseEvent", testParseEvent),
         ("testGetSemesterStartDateWS", testGetSemesterStartDateWS),
@@ -109,6 +123,8 @@ final class LectureAITests: XCTestCase {
         ("testGetFirstLectureDate", testGetFirstLectureDate),
         ("testGetFirstLectureDate2", testGetFirstLectureDate2),
         ("testGetFirstLectureDateSS", testGetFirstLectureDateSS),
-        ("testGetAllLectureDates", testGetAllLectureDates)
+        ("testGetAllLectureDates", testGetAllLectureDates),
+        ("test_dateOfKW",test_dateOfKW),
+        ("test_currentKW", test_currentKW)
     ]
 }
