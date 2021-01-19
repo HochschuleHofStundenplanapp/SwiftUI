51d5ce1a300c46b55e222ed51cb747f8eaa8ed2a
Max <max.grossmann@hof-university.de>
2021-01-19 14:32:03 +0100

    commentsfetcher => commentsextractor

--- CodeSnippets/PureSwift/CommentsFetcher/Sources/CommentsFetcher/CommentsExtractor.swift
+++ CodeSnippets/PureSwift/CommentsFetcher/Sources/CommentsFetcher/CommentsExtractor.swift
@@ -4,7 +4,7 @@
 
 import Foundation
 
-public class CommentsFetcher {
+public class CommentsExtractor {
 
 
     public init () {
--- CodeSnippets/PureSwift/CommentsFetcher/Tests/CommentsFetcherTests/XCTestManifests.swift
+++ CodeSnippets/PureSwift/CommentsFetcher/Tests/CommentsFetcherTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(CommentsFetcherTests.allTests),
+        testCase(CommentsExtractorTests.allTests),
     ]
 }
 #endif
